bdfedf3f1a10d550e84a3bbb2da9ef33b5aebb30
Evgeny Smirnov <smirik@gmail.com>
2011-11-21 22:54:34 +0400

    Fixed deps

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             new Smirik\QuizBundle\SmirikQuizBundle(),
             new Smirik\AdminBundle\SmirikAdminBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
